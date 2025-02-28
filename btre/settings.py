53deaba59793dd1844b2bb913468c98bfbb21dec
Camila <camsan26@bergen.org>
2025-02-28 10:19:50 -0500

    I am stupid

--- btre/settings.py
+++ btre/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['btrealestate-production-74f1.up.railway.app']
 
 
 # Application definition
