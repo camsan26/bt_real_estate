0d5460701b061e9f0316cdc5385e37a0379a95ae
Camila <camsan26@bergen.org>
2025-03-19 11:01:45 -0400

    media url

--- btre/settings.py
+++ btre/settings.py
@@ -168,7 +168,7 @@ STATICFILES_DIRS = [
 ]
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'media/'
+# MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
